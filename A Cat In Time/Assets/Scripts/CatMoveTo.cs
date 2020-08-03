84477a8e96db6c215f938f36b38418b6872222ff
P34nut <linus.ehmann@gmx.de>
2020-08-03 20:40:55 +0200

    dummy cat movement
    movement to the artefact after interacting with the case

---
+++ A Cat In Time/Assets/Scripts/CatMoveTo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CatMoveTo : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    [SerializeField]
+    bool interactedVitrine;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target!=null && interactedVitrine)
+        {
+            agent.SetDestination(target.position);
+            agent.updateRotation = true;
+        }
+    }
+
+
+}
