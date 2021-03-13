2dae0cb9d096a7b6ec29105f035f0d35483766df
SimonStorlSchulke <simonstoschu@googlemail.com>
2021-03-13 10:00:46 +0100

    add candleholder

---
+++ A Cat In Time/Assets/Scripts/CandleHolderModel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CandleHolderModel : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler (0.0f,transform.eulerAngles.y, transform.rotation.z * -1.0f);
+    }
+}
