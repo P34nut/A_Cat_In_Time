e976ea4089437afdc4eb5eab8f8af79eac243ac6
SimonStorlSchulke <simonstoschu@googlemail.com>
2021-04-30 21:01:54 +0200

    stuff

---
+++ A Cat In Time/Assets/Scripts/BlueDot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueDot : MonoBehaviour {
+    public float sec = 14f;
+    public GameObject[] dots = new GameObject[4];
+
+    void Start() {
+        if (gameObject.activeInHierarchy)
+
+            StartCoroutine(showAfterTime());
+    }
+
+    IEnumerator showAfterTime() {
+
+        yield return new WaitForSeconds(sec);
+
+        foreach (GameObject dot in dots)
+        {
+            dot.SetActive(true);
+        }
+    }
+}
